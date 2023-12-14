e945763e7284cf2f5c14bee68e7081b1abe38fc8
Mostafa Ramezani <anonymoustafa@outlook.com>
2023-12-14 15:42:45 -0500

    first api call from node js

--- source/noods/app.js
+++ source/noods/app.js
@@ -1,3 +1,19 @@
 #! /usr/bin/env node
-require('dotenv').config()
-console.log(process.env) // remove this after you've confirmed it is working
+
+
+import OpenAI from "openai";
+import "dotenv/config";
+console.log(process.env.OPENAI_API_KEY) // remove this after you've confirmed it is working
+
+const openai = new OpenAI();
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
